0af31e5017c1ae6c400dae693fc01ec61183b32a
Alex Sanchez <alexsm92.11.25@gmail.com>
2022-06-20 02:05:23 -0500

    Arreglando el codigo

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,17 @@
-console.log("Trinfando como siempre")
+
+console.log("Se nota que todavia me extrañas");
+
+setInterval(cambiarSaludo, 3000);
+
+function cambiarSaludo(){
+    
+    let frase = document.getElementsByTagName("h2")[0].innerHTML ;
+    console.log(+frase);
+if (document.getElementsByTagName("h2")[0].innerHTML == "Ganando como siempre"){
+    document.getElementsByTagName("h2")[0].innerHTML = "Impongo moda para las ninas";
+}
+else {
+    document.getElementsByTagName("h2")[0].innerHTML = "Ganando como siempre";
+}
+console.log("frase : "+ document.getElementsByTagName("h2")[0].innerHTML);
+}
